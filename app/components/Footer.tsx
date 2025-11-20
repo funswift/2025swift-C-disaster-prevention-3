fa4a11094dc67ca2b9525deb61dfeb2d8a3fd5db
Ishizuka Hinata <ihinata0224@gmail.com>
2025-11-20 19:52:33 +0900

    骨組みコピペ+スワイパー
    ヘッダー・フッター・フォント辺りの設定を一旦コピペした
    スワイパーでカードをスワイプできるようにした

---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+// components/Header.tsx
+import React from 'react'
+import Link from 'next/link';
+
+
+const Footer = () => {
+  return (
+    <footer className='bg-blue-300 text-center py-8  text-xl'>
+      <p>公立はこだて未来大学 すうぃふと2025防災グループ</p>
+    </footer>
+  );
+}
+
+export default Footer
+
